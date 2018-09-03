93c4240bbc8c7394223a168a778e5b067c7468d7
kristt26 <kristt26@gmail.com>
2018-09-03 23:30:40 +0900

    New Commid12

--- PertanahanMobileApp/WebApi/wwwroot/apps/js/petugas/petugas.Service.js
+++ PertanahanMobileApp/WebApi/wwwroot/apps/js/petugas/petugas.Service.js
@@ -1,13 +1,13 @@
 angular.module("petugas.services", [])
     .factory('BerkasService', BerkasService)
     ;
-function BerkasService($http, $state, $rootScope, $q, UserServices, MessageServices) {
+function BerkasService($http, $state, $rootScope, $q, MessageServices) {
     let def = $q.defer();
 
     var service = {
         instance: false,
         Berkas: [],
-        get: get, post: post, delete: deleteItem, put: EditItem
+        get: get
     }
     service.get();
     return service;
@@ -17,12 +17,11 @@ function BerkasService($http, $state, $rootScope, $q, UserServices, MessageServi
         if (!this.instance) {
             $http({
                 method: 'Get',
-                url: 'api/Petugas',
-                headers: UserServices.getHeaders()
+                url: 'api/Permohonan/admin'
             }).then(function (response) {
 
                 angular.forEach(response.data, function (value, index) {
-                    service.Petugas.push(value);
+                    service.Berkas.push(value);
                 })
 
                 service.instance = true;
@@ -35,62 +34,8 @@ function BerkasService($http, $state, $rootScope, $q, UserServices, MessageServi
                 def.reject();
             });
         } else
-            def.resolve(this.Petugas);
+            def.resolve(this.Berkas);
 
-        return def.promise;
-    }
-
-    function post(data) {
-        $http({
-            method: 'Post',
-            url: '/Api/Petugas',
-            data: data,
-            headers: UserServices.getHeaders()
-        }).then(function (response) {
-            data.id = response.id;
-            service.Petugas.push(data);
-            MessageServices.success("Data Berhasil Disimpan");
-            def.resolve(data);
-
-        }, function (response) {
-            def.reject();
-            MessageServices.error(response.data);
-        });
-        return def.promise;
-    }
-
-
-    function deleteItem(item) {
-        $http({
-            method: 'delete',
-            url: '/Api/Petugas/' + item.id,
-            data: item,
-            headers: UserServices.getHeaders()
-        }).then(function (response) {
-            var index = service.Kategories.indexOf(item, 1);
-            service.Petugas.splice(index, 1);
-            MessageServices.success("Data Berhasil Dihapus");
-            def.resolve(response.data);
-        }, function (response) {
-            MessageServices.error("Data Tidak Terhapus");
-            def.reject();
-        });
-        return def.promise;
-    }
-
-    function EditItem(item) {
-        $http({
-            method: 'put',
-            url: '/Api/KategoriLayanan/' + item.id,
-            data: item,
-            headers: UserServices.getHeaders()
-        }).then(function (response) {
-            MessageServices.success("Data Tersimpan");
-            def.resolve(response.data);
-        }, function (response) {
-            MessageServices.error("Data Tidak Tersimpan");
-            def.reject();
-        });
         return def.promise;
     }
 }
